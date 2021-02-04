6d7f433a7413e48f8f55e16568f8f7dde97c34c1
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-02-04 11:01:58 +0300

    sweet: switch to aosp bt stack
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     Snap
 
+PRODUCT_COPY_FILES += \
+   frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/a2dp_audio_policy_configuration.xml \
+   frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/bluetooth_audio_policy_configuration.xml
+
 # Device Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
