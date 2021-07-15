812bea37f99b897aac590f7b9b9d93e4974fe36a
rockstar5495 <pratyakshom.05@gmail.com>
2021-07-15 23:35:02 +0530

    sweet: Build with Broken Copy Headers
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,10 @@
 BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/sweet
 
+# Hacks
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
