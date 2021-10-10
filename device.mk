cbdff7831b98977a3118e4636586f1cc289129d8
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-10-10 16:07:42 +0530

    sweet: Build ANXCamera v204 * https://gitlab.com/AdarshGrewal/proprietary_vendor_anxcamera.git
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 
+# Inherit from ANX Camera
+$(call inherit-product, vendor/ANXCamera/anx-vendor.mk)
+
 # The first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 30
 
